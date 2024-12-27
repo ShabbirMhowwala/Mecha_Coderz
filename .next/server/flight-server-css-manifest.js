65338fbc275337e2ea45e514f393f3fdb3ae98a7
Shabbir Mhowwala <148691432+ShabbirMhowwala@users.noreply.github.com>
2024-12-27 17:12:59 +0530

    Add files via upload

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "D:\\projects\\game\\image-alt-generator\\src\\app\\page": [
+      "D:\\projects\\game\\image-alt-generator\\src\\styles\\globals.css",
+      "D:\\projects\\game\\image-alt-generator\\node_modules\\@next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"fontSans\"}"
+    ]
+  },
+  "D:\\projects\\game\\image-alt-generator\\src\\app\\layout.tsx": [
+    "D:\\projects\\game\\image-alt-generator\\node_modules\\@next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"fontSans\"}",
+    "D:\\projects\\game\\image-alt-generator\\src\\styles\\globals.css"
+  ]
+}
---
+++ .next/server/font-loader-manifest.js
@@ -0,0 +1,8 @@
+self.__FONT_LOADER_MANIFEST={
+  "pages": {},
+  "app": {
+    "D:\\projects\\game\\image-alt-generator\\node_modules\\@next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"fontSans\"}": [
+      "static/media/a34f9d1faa5f3315.p.woff2"
+    ]
+  }
+}
